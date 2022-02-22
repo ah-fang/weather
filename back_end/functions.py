4dd4608ced1d3d983b6ef7261e075e0a83699989
BobaRecords <96095517+BobaRecords@users.noreply.github.com>
2022-02-21 22:22:10 -0800

    V1 Complete Weather API to DataFrame
    - Successfully pulls Open Weather API using city id
    - extracts relevant data from json and creates a dataframe

--- back_end/functions.py
+++ back_end/functions.py
@@ -1,31 +1,53 @@
 import requests
-
-def call_data(api_path):
-    print('Attempt to call API')
-    payload = requests.get(api_path)
-    print(payload.json())
-    return payload.json()
+import pandas as pd
 
 
-# def api_configuration(lat, lon, creds):
-#     schema = 'http://'
-#     address = 'api.openweathermap.org/data/2.5/weather'
-#     customizable_parameteres = '?lat=' + str(lat) + '&lon=' + str(lon) + '&appid=' + creds
-#     api_path = schema + address + customizable_parameteres
-#     return api_path
+def create_df():
+    # creates the columns for the final df
+    # needs to be outside the for loop so that it remembers it's values
+    col1 = {
+        'city': [
+                    'weather_description',
+                    'current_temp',
+                    'feels_like',
+                    'high_temp',
+                    'low_temp',
+                    'wind_speed',
+                    'humidity',
+                    'longitude',
+                    'latitude']
+    }
+    df = pd.DataFrame(data=col1)
+    return df
 
-def api_configuration(city_id, creds):
-    schema = 'http://api.openweathermap.org/data/2.5/weather'
-    address = ''
-    query = {'id': city_id, 'appid': creds}
+
+def call_open_weather(city_id, creds, units):
+    # makes the api call and returns json file
+    address = 'http://api.openweathermap.org/data/2.5/weather'
+    query = {'id': city_id, 'appid': creds, 'units': units}
+    response = requests.get(address, params=query)
+    return response.json()
 
 
-    # '?id={city id}&appid={API key}'
-
-
-
-def extract_json(payload):
+def extract_json(payload, df):
+    # extract key info from json and add to df
+    data_city = payload['name']
+    data_weather_description = payload['weather'][0]['description']
+    data_temp = payload['main']['temp']
+    data_feel_temp = payload['main']['feels_like']
+    data_high_temp = payload['main']['temp_max']
+    data_low_temp = payload['main']['temp_min']
+    data_humidity = payload['main']['humidity']
+    data_wind_speed = payload['wind']['speed']
     data_longitude = payload['coord']['lon']
     data_latitude = payload['coord']['lat']
 
-    print(data_latitude)
+    df[data_city] = [data_weather_description,
+                     data_temp,
+                     data_feel_temp,
+                     data_high_temp,
+                     data_low_temp,
+                     data_wind_speed,
+                     data_humidity,
+                     data_longitude,
+                     data_latitude]
--- back_end/main.py
+++ back_end/main.py
@@ -1,28 +1,27 @@
 from functions import *
 from creds import open_weather_key
+from tabulate import tabulate
 
+# List of city IDs
+city_sandiego = '5386053'
+city_bloomington = '4254679'
+city_murrieta = '5375911'
+city_escondido = '5346827'
 
-# api_path = api_configuration(32, 117, open_weather_key)
-# print(api_path)
-# payload = call_data(api_path)
-# extract_json(payload)
+# List of cities that will be looped through
+city_loop = [city_sandiego,
+             city_escondido,
+             city_murrieta,
+             city_bloomington]
 
-creds = open_weather_key
-city_id = '5386053' # san diego, CA
+# temp displayed units, change to metric if you want
+units = 'imperial'
 
-
-address = 'http://api.openweathermap.org/data/2.5/weather'
-query = {'id': city_id, 'appid': creds}
-response = requests.get(address, params=query)
-print(response.json())
-
-
-# City Name
-# weather description
-# feel like temp f
-# High Temp in f
-# Low Temp in f
-# wind speed
-# humidty
-# longitude coordinates
-# latitude coordinates
+#########################################
+######### code starts here ##############
+#########################################
+df = create_df()
+for city_id in city_loop:
+    payload = call_open_weather(city_id, open_weather_key, units)
+    extract_json(payload, df)
+print(tabulate(df, headers='keys', tablefmt='psql', showindex=False, floatfmt='.2f'))
